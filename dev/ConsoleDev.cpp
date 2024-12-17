7a49a02185f3b181f006ac344a25f42cc515f67f
PCwqyy <w123567910@outlook.com>
2024-12-17 20:39:38 +0800

    PClib 24v6a

--- dev/ConsoleDev.cpp
+++ dev/ConsoleDev.cpp
@@ -1,15 +1,15 @@
-#include"../Console.hpp"
+#include"../src/Console.hpp"
 int main()
 {
-	CursorSize(0);
+	// CursorSize(0);
 	SetConsoleOutputCP(CP_GBK);
 	ConTitleA("Test Window");
 	ConSize(20,5);
-	printf("永abcABC123");
-	StartGetMouseInput();
 	while(true)
 	{
-		ColorPosPrintf(0x07,1,1,"%d %d\t\t\t\t",GetMousexy().X,GetMousexy().Y);
+		int fore,back;
+		scanf("%x %x",&fore,&back);
+		ColorPrintfEx(fore,back,"AAAAA\n");
 	}
 	return 0;
 }
